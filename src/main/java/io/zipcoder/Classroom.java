e16e97f7e40c975633f209c20f7d272bc60745a9
Kyle Welch <welch@udel.edu>
2022-07-06 17:49:40 -0400

    classroom average examscore progress

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -2,26 +2,39 @@ package io.zipcoder;
 
 public class Classroom {
 
-    // LEARN WHAT AN OBJECT ARRAY IS AND HOW IT WORKS!!!
-    // learn about hashmaps???
-    //
-
-
-
-    Student[] students;
-
     //INITIALIZERS
+    public Student[] students;
+    int maxNumberOfStudents = 30;
+
+
+    //CONSTRUCTORS
+    public Classroom (){
+        this.students = new Student[30];
+    }
+    public Classroom (int maxNumberOfStudents) {
+        this.students = new Student[maxNumberOfStudents];
+    }
+    public Classroom (Student[] students) {
+        this.students = students;
+    }
 
     //SETTERS
 
     //GETTERS
 
-    public void getStudents() {
-        Student student = new Student();
+    public Student[] getStudents() {
+        return students;
     }
 
 
-    public int getAverageExamScore() { return 10;
+    public double getAverageExamScore() {
+        //iterate through students array, using getAverageExamScore to add to sum,
+        double total = 0;
+
+        for (int i = 0 ; i < students.length ; i++) {
+            total += students[i].getAverageExamScore();
+        }
+        return (total / students.length);
     }
 
 
